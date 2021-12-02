1b8d09c7f053a426d796c8f209287b1481fa3a3f
“naksuk <n4ksuk@gmail.com>
2021-12-02 12:24:55 +0900

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by naksuk on 2021/12/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
